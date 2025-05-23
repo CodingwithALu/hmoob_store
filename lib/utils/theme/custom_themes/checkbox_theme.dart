1c5e7348f083682229ab904521f83c5969ae942c
MuaLu-android <mualu945@gmail.com>
2025-05-23 13:16:59 +0700

    Initial commit

---
+++ lib/utils/theme/custom_themes/checkbox_theme.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+class TCheckboxTheme{
+  TCheckboxTheme._();
+  static CheckboxThemeData lightCheckBoxTheme = CheckboxThemeData(
+
+  );
+}
---
+++ lib/utils/theme/theme.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:t_store/utils/theme/custom_themes/elevated_button_theme.dart';
+import 'package:t_store/utils/theme/custom_themes/text_theme.dart';
+class TAppTheme{
+  TAppTheme._();
+  static ThemeData lightTheme = ThemeData(
+    useMaterial3: true,
+    fontFamily: 'Poppins',
+    brightness: Brightness.light,
+    primaryColor: Colors.blue,
+    scaffoldBackgroundColor: Colors.white,
+    textTheme: TTextTheme.lightTextThemes,
+    elevatedButtonTheme: TElevatedButtonTheme.lightElevatedButtonTheme
+  );
+  static ThemeData darkTheme = ThemeData(
+    useMaterial3: true,
+    fontFamily: 'Poppins',
+    brightness: Brightness.dark,
+    primaryColor: Colors.blue,
+    scaffoldBackgroundColor: Colors.black,
+    textTheme: TTextTheme.darkTextThemes,
+    elevatedButtonTheme: TElevatedButtonTheme.darkElevatedButtonTheme
+  );
+}
