c69b2e02a56e3c5c3555bfb5d5318fba37cf7801
MuaLu-android <mualu945@gmail.com>
2025-06-05 03:41:33 +0700

    update 05-06_2025

--- lib/app.dart
+++ lib/app.dart
@@ -1,18 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
-import 'package:t_store/featrues/authentication/screens/onboarding.dart';
+import 'package:t_store/features/authentication/screens/onboarding/onboarding.dart';
 import 'package:t_store/utils/constants/text_string.dart';
 import 'package:t_store/utils/theme/theme.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
-  static final Uri _productUri = Uri.parse('https://codingwitht.com/ecommerce-app-with-admin-panel/');
-  Future<void> _launchProductLink() async {
-    if (!await launchUrl(_productUri, mode: LaunchMode.externalApplication)) {
-      debugPrint('Could not launch $_productUri');
-    }
-  }
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
