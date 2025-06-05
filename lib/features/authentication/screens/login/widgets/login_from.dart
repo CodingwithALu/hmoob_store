c9dd1e031bdec7716c56298b79d6285bc8af3924
MuaLu-android <mualu945@gmail.com>
2025-06-05 10:21:51 +0700

    update 005_06_2025

---
+++ lib/features/authentication/screens/login/widgets/login_from.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:t_store/features/authentication/screens/signup/signup.dart';
+import 'package:t_store/utils/constants/sizes.dart';
+import 'package:t_store/utils/constants/text_string.dart';
+class TLoginFrom extends StatelessWidget {
+  const TLoginFrom({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          vertical: TSizes.spaceBtwSections,
+        ),
+        child: Column(
+          children: [
+            /// Email
+            TextFormField(
+              decoration: const InputDecoration(
+                prefixIcon: Icon(Iconsax.direct_right),
+                labelText: TTexts.email,
+              ),
+            ),
+            const SizedBox(height: TSizes.spaceBtwInputFields),
+
+            ///Password
+            TextFormField(
+              decoration: const InputDecoration(
+                prefixIcon: Icon(Iconsax.password_check),
+                labelText: TTexts.password,
+                suffixIcon: Icon(Iconsax.eye_slash),
+              ),
+            ),
+            const SizedBox(height: TSizes.spaceBtwInputFields / 2),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    Checkbox(value: true, onChanged: (value) {}),
+                    const Text(TTexts.rememberMe),
+                  ],
+                ),
+                TextButton(
+                  onPressed: () {},
+                  child: const Text(TTexts.forgetPassword),
+                ),
+              ],
+            ),
+            const SizedBox(height: TSizes.spaceBtwSections),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: const Text(TTexts.signIn),
+              ),
+            ),
+            const SizedBox(height: TSizes.spaceBtwSections),
+            SizedBox(
+              width: double.infinity,
+              child: OutlinedButton(
+                onPressed: () => Get.to(() => const SignupScreen()),
+                child: const Text(TTexts.createAccount),
+              ),
+            ),
+            const SizedBox(height: TSizes.spaceBtwSections),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/features/authentication/screens/signup/signup.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:t_store/common/widgets/login_signup/social_buttons.dart';
+import 'package:t_store/features/authentication/screens/signup/widgets/signup_form.dart';
+import 'package:t_store/utils/constants/sizes.dart';
+import 'package:t_store/utils/constants/text_string.dart';
+import 'package:t_store/utils/helpers/helper_functions.dart';
+import '../../../../common/widgets/login_signup/from_divider.dart';
+class SignupScreen extends StatelessWidget {
+  const SignupScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(TSizes.defaultSpace),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              /// Title
+              Text(
+                TTexts.signupTitle,
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+              const SizedBox(height: TSizes.spaceBtwSections),
+              /// From
+              TSignupFrom(),
+              const SizedBox(height: TSizes.spaceBtwSections),
+              ///Divider
+              TFromDivider(dividerText: TTexts.orSignInWith.capitalize!),
+              const SizedBox(height: TSizes.spaceBtwSections),
+              ///Social Buttons
+              const TSocialButtons()
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
